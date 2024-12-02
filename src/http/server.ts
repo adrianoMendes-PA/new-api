e1a302e96e25336216c81835f3fea30bcce65e24
Adriano Mendes <adriano.alyson@gmail.com>
2024-12-01 22:43:05 -0300

    new config server

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,14 +7,5 @@ const app = fastify();
 app.register(createUser);
 app.register(getUsers);
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Server is running at ${address}`);
-  });
-}
-
+// Remova a parte do listen, pois o Vercel gerencia a inicialização do servidor.
 export default app;
