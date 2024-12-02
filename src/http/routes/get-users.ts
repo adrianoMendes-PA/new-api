7ff5f6e3c8c9469ce800f1b8c44f16faf10ed919
Adriano Mendes <adriano.alyson@gmail.com>
2024-12-01 21:46:29 -0300

    get users

---
+++ src/http/routes/get-users.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+
+export async function getUsers(app: FastifyInstance) {
+  app.get("/usuario", async (request, reply) => {
+    try {
+      const usuarios = await prisma.usuario.findMany({
+        select: {
+          id: true,
+          nome: true,
+          cidade: true,
+          estado: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      return reply.status(200).send(usuarios);
+    } catch (error) {
+      console.error("Erro ao buscar usuários:", error);
+      return reply.status(500).send({ error: "Erro ao buscar usuários" });
+    }
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import { createUser } from "./routes/create-user";
+import { getUsers } from "./routes/get-users";
 
 const app = fastify();
 
 app.register(createUser);
+app.register(getUsers);
 
 app.listen({ port: 8080 }).then(() => {
   console.log("Server is running");
