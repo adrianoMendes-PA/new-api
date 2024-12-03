fd6923e6eda9a384e31f3961782032e2394ce588
Adriano Mendes <adriano.alyson@gmail.com>
2024-12-03 18:15:08 -0300

    new routes

--- src/routes/tanque.ts
+++ src/routes/tanque.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createTanque } from "../controllers/tanqueController";
 import { getTanques } from "../controllers/tanqueController";
+import { totalTanques } from "../controllers/tanqueController";
 import { getTanqueById } from "../controllers/tanqueController";
 import { deleteTanque } from "../controllers/tanqueController";
 import authUsuario from "../middleware/auth";
@@ -9,7 +10,8 @@ const tanqueRoutes = Router();
 
 tanqueRoutes.post("/createTanque", authUsuario, createTanque);
 tanqueRoutes.get("/getTanques", authUsuario, getTanques);
-tanqueRoutes.get("/:id", authUsuario, getTanqueById);
+tanqueRoutes.get("/totalTanques", authUsuario, totalTanques);
+tanqueRoutes.get("/:id([0-9a-fA-F-]{36})", authUsuario, getTanqueById);
 tanqueRoutes.delete("/:id", authUsuario, deleteTanque);
 
 export { tanqueRoutes };
