73beaa57d9aa23200a382e1a92f0b912ed66deec
Adriano Mendes <adriano.alyson@gmail.com>
2024-12-03 21:39:55 -0300

    create_table_tanque

---
+++ prisma/migrations/20241204003938_create_table_tanque/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "tanque" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "nomeTanque" TEXT,
+    "largura" DECIMAL(65,30) NOT NULL,
+    "profundidade" DECIMAL(65,30) NOT NULL,
+    "comprimento" DECIMAL(65,30) NOT NULL,
+    "quantPeixe" INTEGER,
+    "tipoPeixe" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tanque_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "tanque" ADD CONSTRAINT "tanque_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
